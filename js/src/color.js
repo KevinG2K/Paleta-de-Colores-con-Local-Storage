cb44b0aa82fc4ae3fc489abd7d29fcc261a06bad
Kevin Gustavo <kg.2k.pl@gmail.com>
2021-10-04 20:19:14 -0400

    Funcionando v2

--- js/src/color.js
+++ js/src/color.js
@@ -1,8 +1,9 @@
 color.value = storage;
 
 //una vez renderizado la pagina llama a la funcion setColor
+getColors();
 setColor();
 
 //toma el selector color, y como parametro tiene una función, y lo que va a retornar
 color.addEventListener("input", setColor);
-color.addEventListener('change', () => saveColor(color.value));
+//color.addEventListener('change', () => saveColor(color.value));
--- js/src/saveColor.js
+++ js/src/saveColor.js
@@ -1,4 +1,9 @@
 //esta función guarda el value en la clave colorValue de la consola.
 const saveColor = (value) => {
-    localStorage.setItem('colorValue', value);  
+    //localStorage.setItem('colorValue', value);  
+    colorList.push(value);
+    localStorage.setItem('colorValue',JSON.stringify(value));
+    localStorage.setItem('colorList',JSON.stringify(colorList));
+
+    getColors();
 };
